82dfe7c20fb2529742c4c738a5a719fe5ca4bbb7
Aziz <97046068+aziz-webcoding@users.noreply.github.com>
2024-04-30 14:21:16 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper', {
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+   },
+   loop: true,
+   autoplay: {
+      delay: 2500,
+      stopOnLastSlide: false,
+      disableOnInteraction: false
+   }
+});
+
